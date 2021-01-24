30405ae5a3701b29a185ace81ef7878e1ed7d00d
SouhailMaraoui <46086002+SouhailMaraoui@users.noreply.github.com>
2021-01-24 13:48:01 -0500

    User Login

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-var formidable = require('formidable');
-
+const formidable = require('formidable');
 const bcrypt = require('bcrypt');
 const axios = require('axios');
 const https = require('https');
@@ -11,49 +10,120 @@ const admin_hostname = '127.0.0.4';
 const verify_hostname = '127.0.0.5';
 const port = 443;
 
-//Read the key and certificat 
+var current_user;		//Current connected user.
+var loginMessage="";	//To display error messages for the user.
+var users = [];			//List of users as read from the database.
+
+//Options for TLS
 const tls_options = 
 {
     key: fs.readFileSync('certificats/key.pem'),
     cert: fs.readFileSync('certificats/cert.pem')
 };
 
+//Options for mTLS
 var mtls_options = 
 {
-    	key: fs.readFileSync('certificats/key.pem'),
-    	cert: fs.readFileSync('certificats/cert.pem'),
-      	requestCert: true,
-      	rejectUnauthorized: false,
-      	ca: [fs.readFileSync('certificats/server_cert.pem')]
+	requestCert: true,
+  	rejectUnauthorized: false,
+  	
+	key: fs.readFileSync('certificats/key.pem'),
+	cert: fs.readFileSync('certificats/cert.pem'),      	
+  	ca: [fs.readFileSync('certificats/server_cert.pem')]
 };
 
 //Create a www. server
 const www_server = https.createServer(tls_options,function(request, response) {
-    //Send status code 200(OK message).
-    response.writeHead(200);
 
-    //Read index.html, return status code 404 in case no such file was found. 
-    fs.readFile('index.html',async function(err, html) {
-        if(err){response.writeHead(404);
-            	response.write("index.html not found");}
-        
-        else	
-        {
+	//Read users.json file 
+	var form = new formidable.IncomingForm();
+    fs.readFile('users.json','utf8',function readFileCallback(err,data){
+		if(err) console.log(err);
+		else users=JSON.parse(data);
+	});
+		
+	//Login Webpage
+	if (request.url=="/login"){
+		//Read login.html, return status code 404 in case no such file was found. 
+		fs.readFile('login.html', function(err, html) {
+        if(err)response.writeHead(404);
+        else {
         	response.write(html);
-        	const pds=["password","password","qwerty"];
-        	try{
-        		for(var pd of pds)
-        		{
-		    		const salt = await bcrypt.genSalt();
-		    		const hashedPassword = await bcrypt.hash(pd, salt)
-		    		console.log(pd+" :: "+hashedPassword);
-        		}
-        	} catch {res.status(500)}
-        	
-        	
+        	response.write(loginMessage);
         }
         response.end();
-    });
+    	});
+	}
+	
+	//Web page to manage user login
+    else if (request.url=="/logingin"){
+		form.parse(request, async function (err, fields) {
+			const usrn=fields.username;
+			const pwd=fields.pwd;
+    		
+    		var usr_exist=false;
+    		
+    		for(var user of users)
+    		{
+    			if(user.usrn===usrn)
+    			{
+    				usr_exist=true;
+    				if(await bcrypt.compare(pwd,user.pwd))
+    				{
+    					loginMessage="";
+    					response.writeHead(301,{Location: 'https://www.coinminers.com/'});
+        				response.end();
+    				}
+    				else
+    				{
+    					loginMessage="Wrong password, Try again."
+						response.writeHead(301,{Location: 'https://www.coinminers.com/login'});
+						response.end();
+    				}
+    			}
+    		}
+    		
+    		if(!usr_exist)
+    		{
+    			const salt = await bcrypt.genSalt();
+				const hashedPassword = await bcrypt.hash(pwd, salt);
+				
+				current_user={
+					"usrn": usrn,
+					"pwd": hashedPassword
+				};
+    		
+    			users.push(current_user);
+				var json= JSON.stringify(users);
+				fs.writeFile("users.json",json,'utf8',function (err){});
+				response.writeHead(301,{Location: 'https://www.coinminers.com/'});
+		    	response.end();
+    		}
+		});
+    }
+    
+    //Signout Webpage
+    else if (request.url=="/signout"){
+    	current_user=null;
+		response.writeHead(301,{Location: 'https://www.coinminers.com/login'});
+		response.end();
+	}
+	
+    //Home Webpage
+    else {
+		if(current_user==null)
+		{
+			response.writeHead(301,{Location: 'https://www.coinminers.com/login'});
+			response.end();
+		}
+		else 
+		{	
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write("Hello "+current_user.usrn);
+			response.write('<br><a href="https://www.coinminers.com/signout">Click here to sign out</a>');
+			response.end();
+		}
+    }
   });
 
 //Create a verify. server (to verify client certificate)
@@ -81,7 +151,6 @@ const verify_server = https.createServer(tls_options, function(request, response
 		response.write('Select Certificate :<br><input type="file" name="uploadedcert"><br><hr>');
 		response.write('Select Key :<br><input type="file" name="uploadedkey"><br><hr>');
 		response.write('<input type="submit">');
-		
 		response.write('</form>');
 		
 		return response.end();
@@ -93,7 +162,6 @@ const admin_server = https.createServer(mtls_options,
     (req, res) => {
     if(req.url == '/authenticate')
     {
-    	console.log("imhere");
 		const cert = req.socket.getPeerCertificate();
 		if (req.client.authorized) {
 			res.write(`Hello ${cert.subject.CN}. your certificate was issued by "${cert.issuer.CN}"!`);
@@ -119,12 +187,7 @@ const admin_server = https.createServer(mtls_options,
 });
 
 //Start the servers on their respective hostname:port
-www_server.listen(port, www_hostname, () => {
-  console.log(`www Server running at https://${www_hostname}:${port}/`);
-});
-admin_server.listen(port, admin_hostname, () => {
-  console.log(`admin Server running at https://${admin_hostname}:${port}/`);
-});
-verify_server.listen(port, verify_hostname, () => {
-  console.log(`verify Server running at https://${verify_hostname}:${port}/`);
-});
+www_server.listen(port, www_hostname, () => {});
+admin_server.listen(port, admin_hostname, () => {});
+verify_server.listen(port, verify_hostname, () => {});
+
